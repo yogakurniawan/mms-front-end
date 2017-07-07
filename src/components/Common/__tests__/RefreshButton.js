a2cfd981ece16b6a04c4b1c9646e2eaee96ebdd1
yogakurniawan <yoga.kwn@gmail.com>
2017-07-07 08:32:13 +0700

    [MMS] fix unit test

--- src/components/Common/__tests__/RefreshButton.js
+++ src/components/Common/__tests__/RefreshButton.js
@@ -5,6 +5,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import RefreshButton from '../RefreshButton';
 
@@ -13,6 +14,8 @@ const refresh = () => {};
 const renderComponent = (props = {}) => mount(
   <MuiThemeProvider><RefreshButton {...props} /></MuiThemeProvider>
 );
+
+injectTapEventPlugin();
 
 describe('<RefreshButton />', () => {
   it('should render a <button> tag', () => {
@@ -23,24 +26,7 @@ describe('<RefreshButton />', () => {
   it('should handle click events', () => {
     const onClickSpy = jest.fn();
     const renderedComponent = renderComponent({ refresh: onClickSpy });
-    renderedComponent.find('a').simulate('click');
+    renderedComponent.find('button').simulate('click');
     expect(onClickSpy).toHaveBeenCalled();
   });
-
-  // it('should have a className attribute', () => {
-  //   const renderedComponent = renderComponent();
-  //   expect(renderedComponent.find('a').prop('className')).toBeDefined();
-  // });
-
-  // it('should not adopt a type attribute when rendering an <a> tag', () => {
-  //   const type = 'text/html';
-  //   const renderedComponent = renderComponent({ href, type });
-  //   expect(renderedComponent.find('a').prop('type')).toBeUndefined();
-  // });
-
-  // it('should not adopt a type attribute when rendering a button', () => {
-  //   const type = 'submit';
-  //   const renderedComponent = renderComponent({ handleRoute, type });
-  //   expect(renderedComponent.find('button').prop('type')).toBeUndefined();
-  // });
 });
